359ac5fd824519372e2e4e9af414545ab4f5e95a
Hiromichi Iizuka <iizuka@eduam.co.jp>
2019-07-18 11:28:36 +0900

    Disable sub, square and cube.

--- test.py
+++ test.py
@@ -13,11 +13,11 @@ def calc():
 def test_add(calc):
     assert calc.add(2, 1) == 3
     
-def test_div(calc):
-    assert calc.div(2, 1) == 2
-
-def test_square(calc):
-    assert calc.square(2) == 4
-    
-def test_cube(calc):
-    assert calc.cube(2) == 8
+#def test_div(calc):
+#    assert calc.div(2, 1) == 2
+#
+#def test_square(calc):
+#    assert calc.square(2) == 4
+#    
+#def test_cube(calc):
+#    assert calc.cube(2) == 8
